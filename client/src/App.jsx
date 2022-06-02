2e1d71f46c9f4ead3a3d06da8cebc35a79906cc7
rugg07 <7hrugvedpawar@gmail.com>
2022-06-02 21:35:17 +0400

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+import {Navbar, Welcome, Footer, Services, Transactions, About} from './components';
+import { TransactionProvider } from './context/TransactionContext';
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+          <Navbar />
+          <TransactionProvider>
+          <Welcome />
+          </TransactionProvider>
+      </div>
+      <About />
+      {/*use transaction provider to wrap transactions component so it can destructure values coming from transaction context without any problem*/}
+      <div className="gradient-bg-welcome">
+      <TransactionProvider> 
+        <Transactions />
+      </TransactionProvider>
+      </div>
+
+      <Services />
+      
+      
+      
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
