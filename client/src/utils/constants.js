54450cc1ba34a2ca4d4dc914a316978b9026418e
rugg07 <7hrugvedpawar@gmail.com>
2022-06-02 21:36:22 +0400

    Create constants.js

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+//the address where our smart_contract was first deployed too.
+export const contractAddress = "0x699508f30358EdBF7ffC0a1f4D355779CC4C9eC1";
