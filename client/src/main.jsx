70095c27caec42a4654b2bb602af7694106abd14
Hrugved <7hrugvedpawar@gmail.com>
2025-01-13 19:01:28 +0400

    Fixed import error TransactionContext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-import {TransactionProvider} from './context/TransactionContext'
+import { TransactionProvider } from './context/TransactionContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <TransactionProvider>
